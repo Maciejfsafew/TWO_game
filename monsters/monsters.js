41e199272890240c48a2f2139ccdc4490284d435
kacpermichalski <kacper.michalski@gmail.com>
2014-11-03 10:46:45 +0100

    [RPG-5] Monster model.

---
+++ monsters/monsters.js
@@ -0,0 +1,23 @@
+var config = [
+    {
+        name: "Tank",
+        description: "Tough but slow.",
+        strength: 10,
+        dexterity: 2,
+        hp: 100,
+        maxhp: 100,
+        items: []
+    },
+    {
+        name: "Rogue",
+        description: "Fast but small hp pool.",
+        strength: 2,
+        dexterity: 10,
+        hp: 30,
+        maxhp: 30,
+        items: []
+    }
+];
+function generateMonster(user, hero) {
+
+}
