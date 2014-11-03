152eb82bafe3340507571ee7ca84542dd2e3dcaa
Wojtek Mielczarek <wojtek.mielczarek@gmail.com>
2014-11-03 23:35:24 +0100

    Basic app server with Express 4 and Primus (engine.io). Ping-pong communication example.

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+console.log("Hello");
+
+// connect to current URL
+var primus = Primus.connect();
+
+primus.on("open", function () {
+    console.log("Connected!");
+})
+
+primus.on("data", function (data) {
+    console.log("Recieved:", data);
+    primus.write(data['message'] + " pong!");
+})
+
