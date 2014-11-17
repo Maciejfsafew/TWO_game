176bc88ef1cb03a81616a731f85330c3aa03ec46
kacper.michalski <kacper.michalski@gmail.com>
2014-11-17 10:55:12 +0100

    [RPG-5] Moved monster definitions to separate file. Added export for monster module.

--- monsters/monsters.js
+++ monsters/monsters.js
@@ -8,57 +8,24 @@ function Monster(monsterConfiguration) {
     this.maxhp = monsterConfiguration.maxhp;
     this.items = monsterConfiguration.items.slice(0);
 }
-var monsterConfig = [
-    {
-        name: "Tank",
-        description: "Tough but slow.",
-        level: 7,
-        strength: 10,
-        dexterity: 2,
-        hp: 100,
-        maxhp: 100,
-        items: []
-    },
-    {
-        name: "Rogue",
-        description: "Fast but small hp pool.",
-        level: 2,
-        strength: 2,
-        dexterity: 10,
-        hp: 30,
-        maxhp: 30,
-        items: []
-    },
-    {
-        name: "Troll",
-        description: "Not very strong, but can take a beating",
-        level: 5,
-        strength: 4,
-        dexterity: 5,
-        hp: 130,
-        maxhp: 130,
-        items: []
-    }
-];
-
-function generateMonster(hero, probability) {
-    //hero - zeby wiedziec gdzie wszedl, map - zeby wiedziec czy pole jest puste
-    var currentField = hero.getCurrentField();
-    if (!containsMonster(currentField)) {
+var monsterDefinitions = require("./monsterDefinitions.json");
+exports.generateMonster = function (hero, probability) {
+    //hero - Must contain currentField property. Probability is of monster generation.
+    if (!containsMonster(hero.currentField)) {
         if (Math.random() <= probability) {
-            var monstersFilteredByLevel = monsterConfig.filter(function (monster) {
+            var monstersFilteredByLevel = monsterDefinitions.filter(function (monster) {
                 return monster.level <= hero.level;
             });
             if (monstersFilteredByLevel.length === 0) {
-                monstersFilteredByLevel = monsterConfig
+                monstersFilteredByLevel = monsterDefinitions
             }
             return new Monster(monstersFilteredByLevel[Math.floor(Math.random() * monstersFilteredByLevel.length)]);
         }
     }
     return null;
-}
+};
 
 
 function containsMonster(field) {
-    return field.type === FieldType.MONSTER && field.monster; // field should probably be able do contain a monster object
+    return field.monster && field.type === FieldType.MONSTER; // field should probably be able do contain a monster object
 }
