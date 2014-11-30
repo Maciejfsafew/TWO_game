2e67066515ae195b3619fee61a888db2b80e0aa6
Michal Nowak <alejajestemmondry@gmail.com>
2014-11-30 23:22:14 +0100

    [CLI] Added example commands file.

---
+++ public/js/cli-commands.js
@@ -0,0 +1,37 @@
+// Example commands
+var Commands = [
+  {
+    name: "map",
+    rest: "/api/map/get",
+  },
+  {
+    name: "north",
+    alias: "n",
+    msg: "You're going north."
+  },
+  {
+    name: "south",
+    alias: "s",
+    msg: "You're going south."
+  },
+  {
+    name: "west",
+    alias: "w",
+    msg: "You're going west."
+  },
+  {
+    name: "east",
+    alias: "e",
+    msg: "You're going east."
+  },
+  {
+    name: "up",
+    alias: "u",
+    msg: "You're going up."
+  },
+  {
+    name: "down",
+    alias: "d",
+    msg: "You're going down."
+  },
+]
