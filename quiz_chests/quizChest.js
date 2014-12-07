577011455a9647bc327e0ea4f480dc2f32c8bfd8
Marek Sadowski <sadowski.marekk@gmail.com>
2014-12-07 23:23:31 +0100

    [RPG-9] Answer checking

--- quiz_chests/quizChest.js
+++ quiz_chests/quizChest.js
@@ -1,12 +1,37 @@
 var FieldType = require("../public/js/fieldTypes.js");
 var quizDefinitions = require("./quizDefinitions.json");
 function Quiz(quizConfiguration) {
+    var that = this;
     this.question = quizConfiguration.question;
     this.answers = quizConfiguration.answers;
-    this.answer = function (answer) {
-        return this.answers[answer].correct
+    this.checkAnswers = function (answerIds) {
+        var result = false;
+        if (answerIds) {
+            if (answerIds) {
+                answerIds().sort();
+                var correctIds = getCorrectAnswerIds().sort();
+                if (answerIds.length === correctIds.length) {
+                    for (var i = 0; i < correctIds.length; i++) {
+                        if(answerIds[i] !== correctIds[i]) {
+                            return false;
+                        }
+                    }
+                    result = true;
+                }
+            }
+            return result;
+        }
+        function getCorrectAnswerIds() {
+            var result = [];
+            that.answers.forEach(function (element, index) {
+                if (element.correct) {
+                    result.push(index);
+                }
+            });
+            return result;
+        }
     }
-}
+};
 
 exports.generateQuiz = function (hero) {
     //hero - Must contain currentField property.
