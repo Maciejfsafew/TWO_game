75c4073732ccdf9cc9cf7123952b7279145464d0
Damian Kudas <damkud@gmail.com>
2015-01-11 16:15:27 +0100

    gold in chest and for killed monster

--- backend/chest.js
+++ backend/chest.js
@@ -3,9 +3,10 @@ var FieldType = require("./fieldTypes")
 function Chest() {
     this.type = FieldType.CHEST;
     this.items = [];
+    this.gold = Math.floor((Math.random() * 2000));
 }
 Chest.prototype.toString = function () {
-    return "Chest<" + this.items + ">";
+    return "Chest<" + this.items + ", gold: " + this.gold + ">";
 };
 
 module.exports = Chest;
--- monsters/monsters.js
+++ monsters/monsters.js
@@ -9,6 +9,7 @@ function Monster(monsterConfiguration) {
     this.hp = monsterConfiguration.hp;
     this.maxhp = monsterConfiguration.maxhp;
     this.items = monsterConfiguration.items.slice(0);
+    this.gold = Math.floor((Math.random() * 200) + 100);
 }
 var monsterDefinitions = require("./monsterDefinitions.json");
 exports.generateMonster = function (hero, probability) {
