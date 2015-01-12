f2e5a12cd0e548c2fc17fd00a49da13ca6a8ed53
bogna <bogna.bb+gh@gmail.com>
2015-01-12 08:18:17 +0100

    display scores as a table, add animation

--- public/js/draw_highscores.js
+++ public/js/draw_highscores.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
-    var shell_panel = document.getElementById('high-scores');
+    var table = document.getElementById('high-scores-table');
+    table = table.tBodies[0];
     primus.send('get_highscores', '', function(response) {
         var highscores = response['highscores'];
         highscores.forEach(function (score) {
-            shell_panel.innerHTML += score.name + " | " + score.score + "<br>";
+            var newRow = table.insertRow(-1);
+
+            var newCell10 = newRow.insertCell(0);
+            var newCellText = document.createTextNode(score.name);
+            newCell10.appendChild(newCellText);
+
+            var newCell01 = newRow.insertCell(1);
+            newCellText = document.createTextNode(score.score);
+            newCell01.appendChild(newCellText);
+
+
+
+
+
         });
+        $('td').typewrite({ 'delay': 100, callback: function() {
+            $("table, td, th").animate({"border-color": "#00fe00"}, 800);
+        }});
+
+
+
     });
+
+
+
+
 });
